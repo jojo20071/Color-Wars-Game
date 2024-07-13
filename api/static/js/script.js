d45f4238e4e9e89763d7a81652035b1aed2faf3f
Johannes Häderle <83701560+jojo20071@users.noreply.github.com>
2024-07-13 20:39:48 +0200

    das

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -59,7 +59,7 @@ async function updateCirclesFromData() {
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      const data = await response.json()[data];
+      const data = await response.json();
       
   
       // Iterate through each key in the data object
