ec2fe9ee6fb406b4a0ecab54ce1ef5fc28d01b7c
Johannes Häderle <83701560+jojo20071@users.noreply.github.com>
2024-07-13 13:25:16 +0200

    try json file as save

--- api/index.py
+++ api/index.py
@@ -1,12 +1,25 @@
 from flask import Flask, send_file, jsonify,render_template
 from flask import request
-
+import json
+import os
 
 app = Flask(__name__)
 
+DATA_FILE = 'data.json'
 
-default_list = ["r",{"f1": 0, "f2": 0, "f3": 0, "f4": 0, "f5": 0, "f6": 0, "f7": 0, "f8": 0, "f9": 0, "f10": 0, "f11": 0, "f12": 0, "f13": 0, "f14": 0, "f15": 0, "f16": 0, "f17": 0, "f18": 0, "f19": 0, "f20": 0, "f21": 0, "f22": 0, "f23": 0, "f24": 0, "f25": 0}]
-data_list = []
+default_json = {"c":"r","f1": 0, "f2": 0, "f3": 0, "f4": 0, "f5": 0, "f6": 0, "f7": 0, "f8": 0, "f9": 0, "f10": 0, "f11": 0, "f12": 0, "f13": 0, "f14": 0, "f15": 0, "f16": 0, "f17": 0, "f18": 0, "f19": 0, "f20": 0, "f21": 0, "f22": 0, "f23": 0, "f24": 0, "f25": 0}
+
+
+def read_data():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'r') as file:
+            return json.load(file)
+    return []
+
+
+def write_data(data):
+    with open(DATA_FILE, 'w') as file:
+        json.dump(data, file)
 
 
 @app.route('/homescreen', methods=['GET'])
@@ -23,7 +36,9 @@ def home2():
 def add_data():
     data = request.json.get('data')
     if data:
+        data_list = read_data()
         data_list.append(data)
+        write_data(data_list)
         return jsonify({"message": "Data added successfully"}), 201
     else:
         return jsonify({"message": "No data provided"}), 400
@@ -31,12 +46,19 @@ def add_data():
 
 @app.route('/list', methods=['GET'])
 def get_list():
+    data_list = read_data()
     return jsonify(data_list), 200
 
 @app.route('/reset', methods=['GET'])
-def reset():
-    data_list = default_list
-    return jsonify(data_list), 200
+def add_data():
+    data = default_json
+    if data:
+        data_list = read_data()
+        data_list.append(data)
+        write_data(data_list)
+        return jsonify({"message": "Data reset sucseffuly"}), 201
+    else:
+        return jsonify({"message": "No data provided"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
