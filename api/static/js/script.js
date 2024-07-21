c4f857de41e59e6568b848422c02aa42540cd5db
Johannes Häderle <83701560+jojo20071@users.noreply.github.com>
2024-07-21 21:12:03 +0200

    dAS

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -198,7 +198,7 @@ function verClick(clickedSquare){
 function handleSquareClick(event) {
   const squareId = parseInt(event.currentTarget.id);
   console.log(squareId,data);
-  if (data["c"] == "r" || data["c"] == "b") {
+  if (data["c"] == localStorage.getItem("color")[1]){
     if (data[`f${squareId}`][0] == 0) {
       verClick(squareId);
     }
