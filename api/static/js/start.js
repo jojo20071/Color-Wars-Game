14bd439063a698abe787ebc114af1f112d38eced
Johannes Häderle <83701560+jojo20071@users.noreply.github.com>
2024-07-30 14:50:12 +0200

    dsaadas

--- api/static/js/start.js
+++ api/static/js/start.js
@@ -1,1 +1,9 @@
-console.log("hi")
+function blue() {
+    localStorage.setItem("color", [1,"b"]);
+    //redirect to /game
+    window.location.href = "/game";
+}
+function red() {    
+    localStorage.setItem("color", [0,"r"]);
+    window.location.href = "/game";
+}
